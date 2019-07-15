b6efc3200dd198605946a054436a2730a3fcf5bd
Perluceo <FullStackDevSMU@gmail.com>
2019-07-14 22:16:18 -0500

    fixed: table shows inventory change from user purchase by adding another column to mysql and including changes to script; cli-table styling for more aesthetic user experience.

--- prompt.js
+++ prompt.js
@@ -1,66 +1,125 @@
-var mysql = require("mysql");
-var inquirer = require("inquirer");
-var Table = require("cli-table");
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+var Table = require('cli-table');
+var table = new Table({
+  head: ['Item ID', 'Item', 'Price', 'Qnty'],
+  colWidths: [20, 40, 15, 15]
+});
+
+
+// CLI Text colors
+var FgBlue = "\x1b[34m";
+var FgWhite = "\x1b[0m";
+var FgCyan = "\x1b[36m";
+var FgGreen = "\x1b[32m";
+var FgMagenta = "\x1b[35m";
 
 var connection = mysql.createConnection({
-	host:"localhost",
-	port:3306,
-	user:"root",
-	password:"password",
-	database:"bamazon"
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "password",
+  database: "bamazon"
 });
 
-connection.connect(function(err){
-	if(err)throw err;
-	console.log("connected as id" + connection.threadId);
+
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
 });
 
-function beginPurchase() {
-	connection.query('SELECT * FROM Products', function(err, res){
-		
-		for(var i = 0; i < res.length; i++){
-			table.push([res[i].item_id, res[i].product_name, res[i].price.toFixed(2), res[i].stock_quantity])
+//Start the appliciton
+time_2Buy();
+
+//Displays products in database table
+function time_2Buy() {
+  connection.query('SELECT * FROM Products', function (err, res) {
+    // Display products and price to user pushed from DB into the CLI-table 
+    for (var i = 0; i < res.length; i++) {
+      table.push([res[i].item_id, res[i].product_name, res[i].price.toFixed(2), res[i].stock_quantity])
     }
     console.log(table.toString());
 
-	inquirer.prompt([{
-		name: "choice",
-		type: "list",
-		message:"Use the arrow key to select what you would like to purchase."
-	},
-	{
-		name:"quantity",
-		type:"input",
-		message:"How many would you wish to purchase?",
-		validate: function (value) {
-			if (isNaN(value) == false) {
-			  return true;
-			} else {
-			  return false;
-			}
-		  }
-	}]).then(function(answer){
+    // Beginning user experience 
+    inquirer.prompt([{
+      
+      name: "choice",
+      type: "list",
+      message: "Use the arrow key to select your product",
+      
+      choices: function (value) {
+        var listArray = [];
+        for (var i = 0; i < res.length; i++) {
+          listArray.push(res[i].product_name);
+        }
+        return listArray;
+      }
+    }, {
+      // User input
+      name: "quantity",
+      type: "input",
+      message: "Great, now that you have picked your product, how many would you like?  Please note the amount available in column labeled Qnty.",
+      validate: function (value) {
+        if (isNaN(value) == false) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+    }]).then(function (answer) {
+      // Gets product object user chose
+      for (var i = 0; i < res.length; i++) {
+        if (res[i].product_name == answer.choice) {
+          var selectedItem = res[i];
+        }
+      }
+      // Calculate remaining stock from purchase
+      var updatedInventory = parseInt(selectedItem.stock_quantity) - parseInt(answer.quantity);
+      var pSales = parseFloat(selectedItem.product_updated).toFixed(2);
+      //console.log (`PSale: ${pSales}`);
+      // If user does not pay attention to Qnty column and orders more than what is in stock, they will be asked if to make another purchase
+      if (selectedItem.stock_quantity < parseInt(answer.quantity)) {
+        console.log(`${FgCyan} Insufficient quantity! ${FgWhite}`);
+        repeat();
+      }
+      // If user wants to purchase an amount in stock, the price will be presented and remaining stock quantity updated in the mysql
+      else {
 
-// refactoring code here to remedy parse error
-//  	var quantityRequested = answers.Quantity;
-//  	var IDrequested = answers.ID;
-//  	purchaseOrder(IDrequested, quantityRequested);
-//  });
-// };
+        // Get total from new purchase, fetch current sales from table and add together.
+        var Total = (parseFloat(answer.quantity) * selectedItem.price).toFixed(2);
+        //console.log(`Total: ${Total}`);
+        //console.log (parseFloat(Total) + parseFloat(pSales)).toFixed(2);
+        var pTotal = (parseFloat(Total) + parseFloat(pSales)).toFixed(2);
+        //console.log(selectedItem.product_updated);
+        var query = connection.query("UPDATE Products SET ?, ? WHERE ?", [{ stock_quantity: updatedInventory }, { product_updated: pTotal }, { item_id: selectedItem.item_id }], function (err, res) {
+          if (err) throw err;
+          console.log(`${FgCyan} Purchase successful! ${FgWhite}`);
+          console.log("Your total is $ " + FgGreen + Total);
+          repeat();
+        });
+      }
 
-// function purchaseOrder(ID, quantityRequested){
-// 	connection.query('Select * FROM products WHERE item_id = ' + ID, function(err,res){
-// 		if(err){console.log(err)};
-// 		if(quantityRequested <= res[0].stock_quantity){
-// 			var totalCost = res[0].price * quantityRequested;
-// 			console.log("Good news your order is in stock!");
-// 			console.log("Your total cost for " + quantityRequested + " " +res[0].product_name + " is " + totalCost + " Thank you!");
+    }); // .then of inquirer prompt
 
-// 			connection.query("UPDATE products SET stock_quantity = stock_quantity - " + quantityRequested + "WHERE item_id = " + ID);
-// 		} else{
-// 			console.log("Sorry we do not have enough in stock " + res[0].product_name + " to complete your order.");
-// 		};
-// 		beginPurchase();
-// 	});
-// };
+  }); // first connection.query of the database
 
+} // time_2Buy function
+
+//Function used to make the experience of the CLI mode like a program. Provides an exit choice to the user.
+function repeat() {
+  inquirer.prompt({
+    // Ask user if he wants to purchase another item
+    name: "repurchase",
+    type: "list",
+    choices: ["Yes", "No"],
+    message: "Would you like to purchase another item?"
+  }).then(function (answer) {
+    if (answer.repurchase == "Yes") {
+      time_2Buy();
+    }
+    else {
+      console.log(`${FgMagenta} Thanks for being a smart shopper, please visit us again! ${FgWhite}`)
+      connection.end();
+    }
+  });
+}
--- workbench/bamazon_seeds.sql
+++ workbench/bamazon_seeds.sql
@@ -1,23 +1,25 @@
 DROP DATABASE IF EXISTS bamazon;
+
 CREATE database bamazon;
+
 USE bamazon;
 
 CREATE TABLE products(
-	item_id INT(4) NOT NULL AUTO_INCREMENT,
-    product_name VARCHAR(100) NOT NULL,
+    item_id INT(4) NOT NULL AUTO_INCREMENT,
+	product_name VARCHAR(100) NOT NULL,
 	department_name VARCHAR(100) NOT NULL,
 	price DECIMAL(10,2) NOT NULL,
 	stock_quantity INT(20) NOT NULL,
+    product_updated DECIMAL (10,2) NULL,
 	PRIMARY KEY (item_id)
 );
 
 Select * FROM products;
 
-INSERT INTO products (item_id, product_name, department_name, price, stock_quantity) 
-VALUES	("6010", "Picasso Magnetic 3D Building Blocks", "Toys", 33.99, 20),
-        ("1010", "Echo Dot Smart Speaker with Alexa Charcoal", "Electronics", 24.99, 19),
-        ("6437", "Alexa Night Vision Camera", "Cameras", 37.98, 16),
-        ("5423", "$25 Playstation Store Gift Card", "Video Games", 25, 15),
-        ("8759", "Where the Crawdads Sing", "Books", 14.99, 9),
-        ("2398", "Women's Racerback Maxi Dress", "Clothing", 24.99, 17)
-            
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity, product_updated ) 
+VALUES  ("6010", "Picasso Magnetic 3D Building Blocks", "Toys", 33.99, 20, 4500),
+        ("1010", "Echo Dot Smart Speaker with Alexa Charcoal", "Electronics", 24.99, 19, 4500),
+        ("6437", "Alexa Night Vision Camera", "Cameras", 37.98, 16, 4500),
+        ("5423", "$25 Playstation Store Gift Card", "Video Games", 25, 15, 4500),
+        ("8759", "Where the Crawdads Sing", "Books", 14.99, 9, 4500),
+        ("2398", "Women's Racerback Maxi Dress", "Clothing", 24.99, 17, 4500)
